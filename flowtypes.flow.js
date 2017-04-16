58f95492c7b71884feb56cfb488286afe51c88cf
dduran1967 <dduran1967@gmail.com>
2017-04-15 21:38:59 -0700

    added flow types

---
+++ flowtypes.flow.js
@@ -0,0 +1,33 @@
+// @flow
+
+export interface Resource {
+  id: string,
+}
+
+declare type Node = {
+  termType: 'NamedNode' | 'BlankNode' | 'Literal' | 'Collection',
+  value: string,
+  substitute(bindings: any): Node,
+  compareTerm(Node): -1 | 0 | 1,
+  equals(Node): boolean,
+  hashString(): Node,
+  sameTerm(Node): boolean,
+  toCanonical(): string,
+  toNT(): string,
+};
+
+declare type NamedNode = {
+  termType: 'NamedNode',
+  value: string,
+};
+
+declare type BlankNode = {
+  termType: 'BlankNode',
+  value: string,
+};
+
+declare type Literal = {
+  termType: 'Literal',
+  lang?: string,
+  datatype?: NamedNode,
+};
