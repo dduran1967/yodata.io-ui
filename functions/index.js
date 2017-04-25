410ca4d10e53a5913e148790cf823213e3b66285
dduran1967 <dduran1967@gmail.com>
2017-04-24 22:20:20 -0700

    fix webhook payload - was sending entire inbox content each execution!

--- functions/index.js
+++ functions/index.js
@@ -8,19 +8,17 @@ const db = admin.database();
 
 // const TEST_URL = 'http://axiosb.in/1mqw97l1';
 
-exports.webhooks = functions.database.ref('/in/{uid}').onWrite(event => {
+exports.webhooks = functions.database.ref('/in/{uid}/{messageId}').onWrite(event => {
   let uid = event.params.uid
-  console.log('uid=',uid);
+  let messageId = event.params.messageId
   let webhook = db.ref(`/user/${uid}/thrume/webhook`);
   return webhook.once('value').then(snap => {
-    console.log(snap.val());
     if (snap.val() === null) {
-      console.log('no webhook, no cry.');
-      return Promise.resolve();
+      return Promise.resolve(); // no active webhook
     } else {
       let hook = snap.val();
       let body = event.data.val();
-      console.log(hook,body)
+      console.log(hook, body)
       return axios.post(hook.url, event.data.val()).then(response => {
         if (response.statusCode >= 400) {
           throw new Error(`HTTP Error: ${response.statusCode}`);
